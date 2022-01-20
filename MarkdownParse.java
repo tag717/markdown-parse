b3102848ce8fe66729b875e26fab635f215236f3
annakkin <515579592@qq.com>
2022-01-19 17:50:49 -0800

    new test

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,16 +11,17 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            //System.out.println(currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             if (nextOpenBracket == -1) break;
+            String closeBracket = markdown.substring(nextCloseBracket+1, nextCloseBracket+2); 
+            if (!closeBracket.equals("(")) break;
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            System.out.println("nextCloseB: " + nextCloseBracket + "; nextOpenB: "+ nextOpenBracket + "; currentIdx" + currentIndex +"\n");
+            //System.out.println("nextCloseB: " + nextCloseBracket + "; nextOpenB: "+ nextOpenBracket + "; currentIdx" + currentIndex +"\n");
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-            
+            System.out.println(currentIndex);
         }
         return toReturn;
     }
